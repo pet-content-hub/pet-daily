6c64af7999f9b9269860aabd2f6f1c76efadad7e
Ray Hu <rayhu007@gmail.com>
2025-08-04 23:49:32 -0700

    更新 vite.config.js 文件以将基础路径设置为根目录，简化项目配置。

--- vite.config.js
+++ vite.config.js
@@ -62,7 +62,7 @@ export default defineConfig({
       }
     }
   },
-  base: process.env.NODE_ENV === 'production' ? '/pet-daily/' : '/',
+  base: '/',
   server: {
     port: 3000,
     open: true,
